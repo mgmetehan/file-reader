6386dbd6c2d241f5eae43f7b3d8c1518d231bc37
mgmetehan <mgmetehan@hotmail.com>
2023-02-27 14:55:33 +0300

    init commit

---
+++ file-reader/src/main/java/com/mgmetehan/filereader/FileReaderApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.filereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileReaderApplication.class, args);
+	}
+
+}
