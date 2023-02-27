7319d22237be3f82b5f6be7f46a51703ca4b6a9d
mgmetehan <mgmetehan@hotmail.com>
2023-02-27 16:41:23 +0300

    Add pdf and word read first 2 byte method

---
+++ file-reader/src/main/java/com/mgmetehan/filereader/type/pdf/PdfReader.java
@@ -0,0 +1,33 @@
+package com.mgmetehan.filereader.type.pdf;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+
+public class PdfReader {
+
+    public void ReadData(String path) {
+        try {
+            PDDocument document = PDDocument.load(new File(path));
+            if (!document.isEncrypted()) {
+                PDFTextStripper stripper = new PDFTextStripper();
+                String text = stripper.getText(document);
+                for (int i = 0; i < 2; i++){
+                    System.out.println(text.charAt(i));
+                }
+            }
+            document.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        PdfReader pdfReader = new PdfReader();
+        var path = "/Users/mgmetehan/Desktop/chomar/C-ProtStajyerproje.pdf";
+        pdfReader.ReadData(path);
+    }
+}
+
---
+++ file-reader/src/main/java/com/mgmetehan/filereader/type/word/WordReader.java
@@ -0,0 +1,31 @@
+package com.mgmetehan.filereader.type.word;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class WordReader {
+    public void ReadDataFirst2Byte(String path) {
+        String result = "";
+
+        try {
+            XWPFDocument doc = new XWPFDocument(Files.newInputStream(Paths.get(path)));
+            List<XWPFParagraph> list = doc.getParagraphs();
+            var x = list.get(0).getParagraphText();
+            for (int i = 0; i < 2; i++) {
+                System.out.println(x.charAt(i));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        WordReader wordReader = new WordReader();
+        var path = "/Users/mgmetehan/Desktop/chomar/deneme.docx";
+        wordReader.ReadDataFirst2Byte(path);
+    }
+}
